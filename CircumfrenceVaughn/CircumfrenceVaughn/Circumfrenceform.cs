f14f2b8469538601823a05af14c19dec0b381b7c
Vaughn Donohue <vaughn.donohue@ocsbstudent.ca>
2020-02-26 13:10:39 -0500

    added code and comment

--- CircumfrenceVaughn/CircumfrenceVaughn/Circumfrenceform.cs
+++ CircumfrenceVaughn/CircumfrenceVaughn/Circumfrenceform.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Vaughn Donohue
+ * Created on: 26-February-2020
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #10 - Name of Program
+ * This program...
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -28,11 +35,28 @@ namespace CircumfrenceVaughn
             //declare the local variable
             double circumfrence, radius;
 
+            //get the radius from the text box
+            radius = double.Parse(txtInput.Text);
+            
+            
+            //calculate the circumfrence
+            circumfrence = 2 * Math.PI * radius;
+
+            // insert circumfence and raius to their respective labels
+            this.lblCircumferenceAnswer.Text = Convert.ToString(circumfrence) + "cm";
+
+
+
+
         }
 
         private void LblCircumferenceAnswer_Click(object sender, EventArgs e)
         {
-            //
+            //this will hide the code
+            this.lblCircumferenceAnswer.Hide();
+
+            // this will show the code
+            this.lblCircumferenceAnswer.Show();
         }
     }
 }
